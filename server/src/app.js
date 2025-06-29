11001a824a71dfe186a6623bc15000ff52725f6b
22120237 <nguyenlenguyen1712004@gmail.com>
2025-06-29 14:57:00 +0700

    fix bug cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,33 @@
-import express from "express"
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 import NewsRouter from "./routes/news.r.js";
 import SummaryRouter from "./routes/summary.r.js";
 import WeatherRouter from "./routes/weather.r.js";
 
+const app = express();
 
-const app = express()
+const allowedOrigins = [
+  "http://localhost:8080",
+  "https://webnews-q5xw.onrender.com"
+];
 
-app.use(express.urlencoded({ extend: true }));
-app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:8080', 'https://webnews-q5xw.onrender.com'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
-app.use('/api/news', new NewsRouter().getRouter())
-app.use('/api/summary', new SummaryRouter().getRouter())
-app.use('/api/weather', new WeatherRouter().getRouter())
+app.use(express.urlencoded({ extended: true }));  
+app.use(express.json());
 
-export default app
+app.use('/api/news', new NewsRouter().getRouter());
+app.use('/api/summary', new SummaryRouter().getRouter());
+app.use('/api/weather', new WeatherRouter().getRouter());
+
+export default app;
