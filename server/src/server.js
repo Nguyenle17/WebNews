74247a858a1401b41c5f9b5f70a2b8892183e5d6
22120237 <nguyenlenguyen1712004@gmail.com>
2025-06-29 15:04:39 +0700

    fix bug

--- server/src/server.js
+++ server/src/server.js
@@ -4,8 +4,7 @@ import dotenv from "dotenv";
 dotenv.config('.env');
 
 const port = process.env.PORT || 8081;
-const hostname = process.env.HOSTNAME || "127.0.0.1";
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`)
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
