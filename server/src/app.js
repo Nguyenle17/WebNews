18cc27a1dea8b3772b376f84a3d4ac619986c26c
22120237 <nguyenlenguyen1712004@gmail.com>
2025-06-29 14:42:27 +0700

    fix cors

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.urlencoded({ extend: true }));
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://webnews-q5xw.onrender.com'],
+  origin: ['http://localhost:8080', 'https://webnews-q5xw.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
