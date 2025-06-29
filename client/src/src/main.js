029933e5551456ad4611eb4c7731ebbafb239dae
22120237 <nguyenlenguyen1712004@gmail.com>
2025-06-29 14:15:03 +0700

    remove leaflet dependency

--- client/src/src/main.js
+++ client/src/src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import router from './router'
 import { createPinia } from 'pinia';
 import '../src/assets/css/style.css'
-import 'leaflet/dist/leaflet.css';
 import App from './App.vue'
 
 const app = createApp(App);
